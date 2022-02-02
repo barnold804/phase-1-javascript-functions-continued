d7f08e7e42612d81294b39d36de6b47a07e05504
Brian Arnold <b.arnold09@gmail.com>
2022-02-02 12:18:33 -0500

    commit tree c18c12f1852edc700b28236b4f3fee2793bf32df

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(defaultActivity = "roller-skate") {
+    return (`This Saturday, I want to ${defaultActivity}!`)
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(defaultActivity = "go to the office") {
+    return (`This Monday, I will ${defaultActivity}.`)
+}
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return function innerFunction(special = "special") {
+        return `You are ${flair}${special}${flair}!`
+    }
+
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
